6f531d6e7192374e7618df75055b57a9271fad02
AntonJava23 <antbri1009@skola.goteborg.se>
2024-11-15 10:14:35 +0100

    Made some exception changes

--- src/main/java/com/yrgo/services/customers/CustomerNotFoundException.java
+++ src/main/java/com/yrgo/services/customers/CustomerNotFoundException.java
@@ -4,10 +4,6 @@ public class CustomerNotFoundException extends Exception {
 	// this is just to stop the annoying warning in Eclipse.
 	private static final long serialVersionUID = 1L;
 
-	public CustomerNotFoundException(String message) {
-		super(message);
-	}
-
 	public CustomerNotFoundException(String message, Throwable cause) {
 		super(message, cause);
 	}
